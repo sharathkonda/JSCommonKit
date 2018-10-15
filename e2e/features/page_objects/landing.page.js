5cb55ce315c019c965a56ac1fa8b0063b870c935
Sharath Konda <sharathkonda@gmail.com>
2018-10-15 23:21:20 +0100

    thin fw

--- e2e/features/page_objects/landing.page.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-import * as cml from '../support/_common_library';
-
-const elements = new Map().set('title', element(by.css('#prm > div > span')));
-
-export let title = {
-	waitForVisibility: async () => await cml.waitForElementToBeVisible(elements.get('title'), 5000, 'landing.title.waitForVisibility'),
-	getText: async () => await cml.getElementText(elements.get('title'), false, false, 'landing.title.getText')
-};
